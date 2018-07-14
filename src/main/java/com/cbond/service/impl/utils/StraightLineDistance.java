dae1c0b8f065059e9c6f1ee347c9e782b3e13b45
Sortinn <sortinndecar@gmail.com>
2018-07-14 21:42:18 +0800

    小修改

--- src/main/java/com/cbond/service/impl/utils/StraightLineDistance.java
+++ src/main/java/com/cbond/service/impl/utils/StraightLineDistance.java
@@ -13,31 +13,6 @@ public class StraightLineDistance implements Distance {
         Double latitude2 = location2.getLatitude();
         Double longitude2 = location2.getLongitude();
 
-        // Radius of the earth in km
-//        int R = 6371;
-//        Double dLat = deg2rad(latitude2 - latitude1);
-//        Double dLon = deg2rad(longitude2 - longitude1);
-//        Double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-//                Math.cos(deg2rad(latitude1)) * Math.cos(deg2rad(latitude2)) *
-//                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-//        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-//        Double d = R * c;
-//
-//        return d;
-
-//        double theta = longitude1 - longitude2;
-//        double dist = Math.sin(deg2rad(latitude1)) * Math.sin(deg2rad(latitude2)) + Math.cos(deg2rad(latitude1)) * Math.cos(deg2rad(latitude2)) * Math.cos(deg2rad(theta));
-//        dist = Math.acos(dist);
-//        dist = rad2deg(dist);
-//        dist = dist * 60 * 1.1515;
-//        if (unit.equals("K")) {
-//            dist = dist * 1.609344;
-//        } else if (unit.equals("N")) {
-//            dist = dist * 0.8684;
-//        }
-//
-//        return (dist);
-
         final int R = 6371; // Radius of the earth
 
         double latDistance = Math.toRadians(latitude2 - latitude1);
@@ -53,14 +28,4 @@ public class StraightLineDistance implements Distance {
 
         return Math.sqrt(distance);
     }
-
-
-    private static double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-    }
-
-    private static double rad2deg(double rad) {
-        return (rad * 180 / Math.PI);
-    }
-
 }
